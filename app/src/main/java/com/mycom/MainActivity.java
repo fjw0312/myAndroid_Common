93a25235a5e599efccebacfbd91da40ada6b6407
fjw0312 <fjw0312@163.com>
2018-03-13 22:13:00 +0800

    工程创建-fang

---
+++ app/src/main/java/com/mycom/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mycom;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*****
+ * author:fjw0312
+ * date: 2018.3.13
+ * 功能说明： 个人 公共使用模块 整合 迭代工程
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
